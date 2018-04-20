d6d8b25499d7cdf082a103bc0e758c5501939be9
Connor Hargus <cjh2209@columbia.edu>
2018-04-20 17:41:17 -0400

    Add warmup, joint training, test runs, training on different minigames

--- run_loop.py
+++ run_loop.py
@@ -5,23 +5,47 @@ from __future__ import print_function
 import time
 
 
-def run_loop(agents, env, max_frames=0):
+def run_loop(agents, env, max_frames=0, mlsh=False, warmup=2, joint=8):
   """A run loop to have agents and an environment interact."""
   start_time = time.time()
 
-  avg_scores = []
-
   try:
+    # Store episode number for this thread
+
+    num_test_runs = 50
     num_ep = 0
+
     while True:
       num_frames = 0
       timesteps = env.reset()
 
-      # Note that agents is just a list of a single agent in this case
+      # Note: agents is just a list of a single agent in this case
       for a in agents:
         a.reset()
-        a.training = not ((num_ep % 30) == 0)
-        print('Training: ' + str(a.training))
+
+        # Warm-up period where only master policy is trained of "warmup" episodes
+        if mlsh and (num_ep % (warmup + joint + num_test_runs) < warmup):
+          a.train_only_master = True
+          a.test_run = False
+
+          # Ended  joint training, reset master value and subpol choice parameters
+          if (num_ep % (warmup + joint + num_test_runs)) == 0:
+            print("\nResetting master policy")
+            a.reset_master()
+
+          print('\nWarming Up...')
+
+        # Joint training period where both master and subpolicies trained of "joint" episodes
+        elif mlsh and (num_ep % (warmup + joint + num_test_runs) < warmup + joint):
+          a.train_only_master = False
+          a.test_run = False
+
+          print('\nJoint Training...')
+
+        # Do test runs after joint training has finished
+        elif mlsh:
+          a.test_run = True
+          print('\nTesting...')
 
       num_ep += 1
 
@@ -30,8 +54,9 @@ def run_loop(agents, env, max_frames=0):
         last_timesteps = timesteps
         actions = [agent.step(timestep) for agent, timestep in zip(agents, timesteps)]
         timesteps = env.step(actions)
-        # Only for a single player!
-        is_done = (num_frames >= max_frames and agents[0].training) or timesteps[0].last()
+
+        # Done if exceeded max_frames and training run, or finished full minigame episode
+        is_done = (num_frames >= max_frames and not agents[0].test_run) or timesteps[0].last()
         yield [last_timesteps[0], actions[0], timesteps[0]], is_done
         if is_done:
           break
